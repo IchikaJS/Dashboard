d328c80af284a3b6afa734d5c6235324ce06fbd6
Dylan Ogle <delxhq@funo.network>
2019-12-22 05:43:28 +0000

    Remove debugging code.

--- src/util/socket.js
+++ src/util/socket.js
@@ -1,10 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3000', socket => {
-  socket.emit('guilds', data => {
-    console.log(data)
-  })
-})
+const socket = io('http://localhost:3000')
 
 export class Socket {
   
